e91a9b261532adc1674d0127da77c751eb75b858
fugo4121 <lancerevo345@yahoo.co.jp>
2020-08-02 20:47:18 +0900

    ver old

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,5 @@
 class TweetsController < ApplicationController
-#lkjfdaj444443333hrtrwaaa
-# ver1
-# ver2
+#lkjfdaj444443333hrtrw
   # editとshowの、対象id投稿データを@tweetに入れる処理が同じため、まとめる
   before_action :set_tweet, only: [:edit, :show]
 
